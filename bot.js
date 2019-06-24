3e5a66110307860e00e7b522a1b8b6495e4a3876
Redyx42 <52102656+Redyx42@users.noreply.github.com>
2019-06-24 20:28:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client(560167629885669379);
 
 client.on('ready', () => {
     console.log('I am ready!');
